37873ccb38fdb19a899c92c02d576752434cce23
中野雄貴 <totoro.windows@outlook.com>
2018-06-14 18:29:56 +0900

    vueでjson

--- kosensai/js/main.js
+++ kosensai/js/main.js
@@ -1,5 +1,5 @@
 // タイムテーブル
-axios.get('http://localhost:8080/json/timetable.json').then(function (response) {
+axios.get('../json/timetable.json').then(function (response) {
     initVue(response.data);
 }).catch(function (error) {
     console.log(error);
@@ -9,7 +9,13 @@ function initVue(info){
     new Vue({
         el: '#app',
         data: {
-            infos: info["http://uedayou.net/loa/東京都千代田区永田町一丁目7"]
+            infos: info["timetable"]
+        }
+    });
+    new Vue({
+        el: '#shop',
+        data: {
+            shops: info["shop"]
         }
     })
 }
