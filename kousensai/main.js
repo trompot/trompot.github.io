b3f78cf382a9e334124571954aa026e133fa22af
trompot <trompot.mydns.jp@gmail.com>
2018-06-18 21:05:58 +0900

    Add files via upload

--- kousensai/main.js
+++ kousensai/main.js
@@ -1,6 +1,12 @@
 
 $(function(){
-    $(".loading").on('webkitAnimationEnd', function(){
-        $(".loading").remove();
+    $('.loading').on('webkitAnimationEnd', function(){
+        $('.loading').remove();
     });
+});
+
+// powerの色変える
+$('button').click(function(){
+    $(this).toggleClass('fs-power-red');
+    $(this).attr('disabled', true);
 });
