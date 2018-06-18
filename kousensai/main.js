089fb619fd43c01fc35943548a5a2c1af7da8fb5
trompot <trompot.mydns.jp@gmail.com>
2018-06-18 22:36:41 +0900

    Add files via upload

--- kousensai/main.js
+++ kousensai/main.js
@@ -1,12 +1,13 @@
 
 $(function(){
     $('.loading').on('webkitAnimationEnd', function(){
-        $('.loading').remove();
+        // $('.loading').remove();
     });
 });
 
 // powerの色変える
 $('button').click(function(){
     $(this).toggleClass('fs-power-red');
+    $(this).toggleClass('fs-power-white');
     $(this).attr('disabled', true);
 }).css('cursor','pointer');
