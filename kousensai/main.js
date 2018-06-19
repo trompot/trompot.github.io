325e27493319601fe74d8ccedbc6ec5daa467522
trompot <trompot.mydns.jp@gmail.com>
2018-06-20 07:04:16 +0900

    Add files via upload

--- kousensai/main.js
+++ kousensai/main.js
@@ -1,5 +1,11 @@
 
+// スクロールを無効にする
+$(window).on('touchmove.noScroll', function(e) {
+    e.preventDefault();
+});
+
 $(function(){
+    // 時差で発動する
     setTimeout(function(){
         $('button').click();
     }, 2000);
@@ -7,6 +13,10 @@ $(function(){
     $('#load').on('webkitAnimationEnd', function(){
         $('#load').remove();
         $('button').remove();
+        $('body').removeAttr('overflow-y');
+        
+        // スクロール無効を解除する
+        $(window).off('.noScroll');
     });
 });
 
@@ -14,10 +24,11 @@ $(function(){
 $('.btn-origin').click(function(){
     $(this).toggleClass('fs-power-red');
     $(this).toggleClass('fs-power-white');
-    $(this).attr('disabled', true);
     
     // window.navigator.vibrate(200);
     setTimeout(function(){
         $('#load').toggleClass('loading');
     }, 500);
 });
+
+
